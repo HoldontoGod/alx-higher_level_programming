62cce46eeb194229432f607c5ab5a1eb244404f1
HoldontoGod <simbeyemonica@gmail.com>
2023-01-05 16:46:39 -0800

    Hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
 
 if __name__ == "__main__":
-   file = dir(hidden_4)
-   length = len(file)
-   for i in range(0, length):
-       if file[i][0:2] != "__":
-            print(file[i])
+    """Print all names defined by hidden_4 module"""
+    import hidden_4
+    
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
+
