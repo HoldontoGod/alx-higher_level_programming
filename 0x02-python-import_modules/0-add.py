2426bf54dcdb14e434bf464799a3dff0f6aeaa25
HoldontoGod <simbeyemonica@gmail.com>
2023-01-05 18:12:05 -0800

    Python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,7 @@
 if __name__ == "__main__":
     """Print some of 1 and 2"""
     from add_0 import add
-
-
+    
     a = 1
     b = 2
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
