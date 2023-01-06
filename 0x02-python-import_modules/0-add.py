895ea7aefcb3925ab375c03309331c3114fa2953
HoldontoGod <simbeyemonica@gmail.com>
2023-01-05 17:41:55 -0800

    Magic_calculation.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
+    """Print some of 1 and 2"""
     from add_0 import add
 
-     a = 1
-     b = 2
 
-     a = int(a)
-     b = int(b)
-
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
